7eb7a5acaad061301726201e0b3f906b822e0cfe
KyleDeveaux <kyledeveaux@gmail.com>
2025-01-23 12:12:24 -0500

    Updated code for ultimte pass of test

--- Categorize_New_Member/Code.js
+++ Categorize_New_Member/Code.js
@@ -2,7 +2,7 @@ function openOrSenior(data){
     let newMembers = [];
     
     for(let i = 0; i < data.length; i++){
-      data[i][0] < 55 || data[i][1] < 7 ? newMembers[i] = "Open" : newMembers[i] = "Senior"
+      data[i][0] < 55 || data[i][1] <= 7 ? newMembers[i] = "Open" : newMembers[i] = "Senior"
     }
     return newMembers
   }
